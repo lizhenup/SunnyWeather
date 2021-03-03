8729ea199193815d660c8359a3d2ecf14e1a49a6
lizhenup <645876815@qq.com>
2021-03-03 23:10:55 +0800

    实现搜索全球城市数据功能

---
+++ app/src/main/java/com/android/sunnyweather/SunnyWeatherApplication.kt
@@ -0,0 +1,18 @@
+package com.android.sunnyweather
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication : Application() {
+    companion object {
+        const val TOKEN = "还没拿到彩云令牌"
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
